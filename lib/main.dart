948bedad69c8c04e7a4ac9330a2b9000e8ccf008
LeoOliva0 <cassio_oliva@hotmail.com>
2023-04-01 16:32:42 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+
+main() => runApp(new PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp>{
+var _perguntaSelecionada = 0;
+
+  void _responder(){
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
+@override
+  Widget build(BuildContext context){
+    final  perguntas = [
+       'Qual é a sua cor favorita?',
+       'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar( 
+          title:Text('Perguntas'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget> [
+            Text(perguntas[0]),
+          ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+      );
+  }
+}
+
+class PerguntaApp extends StatefulWidget{
+  
+  _PerguntaAppState createState(){
+    return _PerguntaAppState();
+  }
+}
